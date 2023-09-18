17bc56749964b43a76f8c5fb762ea94e74b63a6b
jorafael3 <jalvaradoe3@gmail.com>
2023-09-17 23:54:14 -0500

    ZX

---
+++ models/menumodel.php
@@ -0,0 +1,54 @@
+<?php
+
+
+class MenuModel extends Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    function Cargar_Menu($param)
+    {
+        try {
+            $query = $this->db->connect_dobra()->prepare("SELECT us.Nombre,sm.Nombre,ss.sub_nombre,
+            sm.ruta,
+            sm.vista,
+            ss.ruta as sub_ruta,
+            ss.vista as sub_vista,
+            ss.variable as sub_variable,
+            sm.variable as menu_variable,
+            sm.icono as icono_menu,
+            case
+                when sm.vista is not null  or sm.vista != '' then 1 else 0 
+            end as Ismenu,
+            case
+	            when (sm.vista is null  or sm.vista = '') and (ss.ruta is null or ss.ruta = '')  then 1 else 0 
+                end as Ismenu_Drop,
+            case
+                when ss.sub_nombre is null or ss.sub_nombre = '' then 0 else 1 
+            end as IsSubmenu,
+            acc.*
+            from SIS_USUARIO_ACCESOS acc
+            left join USUARIOS us on us.usuario_ID = acc.usuario_ID
+            left join sis_menu sm on sm.menu_ID = acc.menu_ID 
+            left join sis_submenu ss on ss.submenu_ID = acc.submenu_ID 
+            where us.usuario_ID = 1
+                ");
+            if ($query->execute()) {
+                $result = $query->fetchAll(PDO::FETCH_ASSOC);
+                echo json_encode($result);
+                exit();
+            } else {
+                $err = $query->errorInfo();
+                echo json_encode($err);
+                exit();
+            }
+        } catch (PDOException $e) {
+            $e = $e->getMessage();
+            echo json_encode($e);
+            exit();
+        }
+    }
+}
