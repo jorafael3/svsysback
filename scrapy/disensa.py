ec0b9d0b7f7c39f98188fe3ec26c9dc07e3ddd3f
jorafael3 <jalvaradoe3@gmail.com>
2023-09-21 15:52:58 -0500

    asd

--- scrapy/disensa.py
+++ scrapy/disensa.py
@@ -14,9 +14,10 @@ import math
 import os
 import PyPDF2
 from PyPDF2 import PdfReader
-import tabula
 import re
 import zipfile
+import mysql.connector
+from datetime import datetime
 
 options = Options()
 options.add_argument("--start-maximized")
@@ -29,6 +30,12 @@ options.add_experimental_option("prefs", prefs)
 ser = Service()
 driver = webdriver.Chrome(service=ser, options=options)
 # driver = webdriver.Chrome()
+conexion = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="svsys"
+    )
 
 def login():
     driver.get('https://www.portaldisensa.com/b2b_new/init.do')
@@ -142,12 +149,10 @@ def Obtener_cabecera(cabecera):
         datos = {}
         # Divide el texto en líneas
         lineas = cabecera.split('\n')
-
         # Itera a través de las líneas y agrega los datos al diccionario
         for linea in lineas:
             # Divide la línea en clave y valor usando el primer ':' como separador
             partes = linea.split(':', 1)
-            
             # Si hay al menos dos partes (clave y valor), agrega al diccionario
             if len(partes) == 2:
                 clave = partes[0].strip()
@@ -156,23 +161,41 @@ def Obtener_cabecera(cabecera):
         # print(datos)
         # Imprime el diccionario
         datos_a = {}
-        campos = ["FACTURA","FECHA EMISIÓN","FECHA DE EMISIÓN","TIPO DE ENTREGA","TELÉFONO","FECHA VALIDEZ","PEDIDO INTERNO","PED. COMPRA"]
+        campos = ["FECHA EMISIÓN",
+                  "FECHA DE EMISIÓN",
+                  "TIPO DE ENTREGA",
+                  "TELÉFONO",
+                  "FECHA VALIDEZ",
+                  "TIPO DE ENTREGA",
+                  "PEDIDO INTERNO",
+                  "PED. COMPRA",
+                  "FACTURA"]
+        
         for clave, valor in datos.items():
-            print(f'{clave}: {valor}')
-            time.sleep(0)
-            # for campo in campos:
-            if valor in campos:
-            #    partes = valor.split(campo)
-               print(partes)
-               campo1 = partes[0].strip()
-               campo2 = partes[1].strip()
-            #    datos_a[clave.replace(" ","_")] = valor
-               datos_a[valor.replace(" ","_")] = campo2
-            # else:
-            #    datos_a[clave.replace(" ","_")] = valor
+                for campo in campos:
+                    indice_fecha_emision = valor.find(campo)
+                    if indice_fecha_emision != -1:
+                        campo1 = valor[:indice_fecha_emision].strip()
+                        campo2 = valor[indice_fecha_emision + len(campo) + 1:].strip()
+                        datos_a[clave] = campo1
+                        datos_a[campo] = campo2
+                    else:
+                        if clave in datos_a:
+                            pass
+                        else:
+                            datos_a[clave] = valor
 
+        datos_b = {}
+        for clave, valor in datos_a.items():
+            clave = clave.strip()
+            clave  = clave.replace('.', '')
+            clave  = clave.replace(' ', '_')
+            valor = valor.replace(":",'')
+            if(clave =="FECHA_EMISIÓN" or clave =="FECHA_DE_EMISIÓN"):
+                clave = "FECHA_EMISIÓN"
+            datos_b[clave] = valor
 
-        return datos_a
+        return datos_b
 
 def Leer_pdf():
 
@@ -211,7 +234,100 @@ def Leer_pdf():
                 # print(datos_cabecera)
                 lista = [datos_cabecera,datos_tabla,]
                 array_datos.append(lista)
-    
-    print(array_datos)
+    print("*******************************************")
+    # print(array_datos)
+    Guardar_Guias(array_datos)
+
+def Guardar_Cabecera(datos):
+    cursor = conexion.cursor()
+    consulta = """
+        INSERT INTO GUIAS (
+            FECHA_DE_EMISION,
+            FACTURA,
+            TELEFONO,
+            FECHA_VALIDEZ,
+
+            CLIENTE,
+            CLIENTE_RUC,
+            SOLICITANTE,
+            DIRECCION_1,
+
+            PTO_DE_PARTIDA,
+            PTO_DE_LLEGADA,
+            DIRECCION_2,
+
+            TIPO_DE_ENTREGA,
+            PEDIDO_INTERNO,
+            PED_COMPRA
+            ) VALUES (
+                %s, %s, %s , %s,
+                %s, %s, %s , %s,
+                %s, %s, %s , 
+                %s, %s, %s
+                )
+    """
+    fecha_emision = datetime.strptime(datos["FECHA_EMISIÓN"].strip(), "%d.%m.%Y")
+    fecha_emision = fecha_emision.strftime("%Y/%m/%d")
+
+    fecha_val = datetime.strptime(datos["FECHA_VALIDEZ"].strip(), "%d.%m.%Y")
+    fecha_val = fecha_val.strftime("%Y/%m/%d")
+
+    valores = (
+            fecha_emision, 
+            datos["FACTURA"].strip(),           
+            datos["TELÉFONO"].strip(),
+            fecha_val,
+
+            datos["CLIENTE"].strip(),
+            datos["RUC"].strip(),
+            datos["SOLICITANTE"].strip(),
+            datos["DIRECCION"].strip(),
+
+            datos["PTO_DE_PARTIDA"].strip(),
+            datos["PTO_DE_LLEGADA"].strip(),
+            datos["DIRECCIÓN"].strip(),
+
+            datos["TIPO_DE_ENTREGA"].strip(),
+            datos["PEDIDO_INTERNO"].strip(),
+            datos["PED_COMPRA"].strip(),
+    )
+    try:
+    # Intenta ejecutar la consulta con los valores
+        cursor.execute(consulta,valores)
+        # resultados = cursor.fetchall()
+        # for fila in resultados:
+        #     print(fila)
+    # Realiza la confirmación para guardar los cambios en la base de datos
+        conexion.commit()
+        print("Inserción exitosa")
+    except Exception as e:
+        # Captura cualquier excepción que ocurra durante la inserción
+        print("Error durante la inserción:", str(e))
+    finally:
+        # Cierra el cursor y la conexión
+        cursor.close()
+
+
+
+
+def Guardar_Guias(array_datos):
+    # print(array_datos)
+#     cursor = conexion.cursor()
+#     consulta = "SELECT * FROM usuarios"
+#     cursor.execute(consulta)
+#     resultados = cursor.fetchall()
+#     for fila in resultados:
+#         print(fila)
+# # Cierra el cursor y la conexión
+#     cursor.close()
+#     conexion.close()
+    for row in array_datos:
+        cabecera = row[0]
+        detalle =  row[1]
+        Guardar_Cabecera(cabecera)
+        # for val in row[0]: 
+        #     print(val[0]["MATRIZ"])
+
 Leer_pdf()
 
+ 
