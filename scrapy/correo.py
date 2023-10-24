98f40a42b01ecc841c32949b3ea1b45198d9bdc6
jorafael3 <jalvaradoe3@gmail.com>
2023-10-24 17:57:17 -0500

    dfg

---
+++ scrapy/correo.py
@@ -0,0 +1,101 @@
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from datetime import datetime, timedelta
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+credential = 'C:\\xampp\\htdocs\\svsysback\\scrapy\\client_secret.json'
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    start_date_str = ''  # Inicializa las variables
+    end_date_str = ''
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                credential, SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+        end_date = datetime.utcnow()
+        print(end_date)
+        start_date = end_date - timedelta(days=7)
+
+        # Convierte las fechas a formato RFC3339
+        start_date_str = start_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+        end_date_str = end_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    try:
+        # Call the Gmail API
+        service = build('gmail', 'v1', credentials=creds)
+        results = service.users().messages().list(
+            userId='me', 
+            labelIds=['INBOX'],
+            # q=f'after:{start_date_str} before:{end_date_str}'
+        ).execute()
+        messages = results.get('messages', [])
+
+        if not messages:
+            print('No messages found in the inbox.')
+            return
+
+        print('Messages in the inbox:')
+        for message in messages:
+            msg = service.users().messages().get(userId='me', id=message['id']).execute()
+            # print(msg)
+            print(msg["snippet"])
+
+            # print(f'Subject: {msg["subject"]}')
+            # print(f'From: {msg["from"]}')
+            # print(f'Date: {msg["internalDate"]}')
+
+    except HttpError as error:
+        # TODO(developer) - Handle errors from gmail API.
+        print(f'An error occurred: {error}')
+
+
+if __name__ == '__main__':
+    main()
+
+
+# from google_auth_oauthlib.flow import InstalledAppFlow
+# from googleapiclient.discovery import build
+
+# # Define el alcance de acceso a Gmail
+# SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+# flow = InstalledAppFlow.from_client_secrets_file('C:\\xampp\\htdocs\\svsysback\\scrapy\\client_secret.json', SCOPES)
+# creds = flow.run_local_server(port=0)
+
+
+# service = build('gmail', 'v1', credentials=creds)
+
+# results = service.users().messages().list(userId='me', labelIds=['INBOX']).execute()
+# messages = results.get('messages', [])
+
+# if not messages:
+#     print('No se encontraron mensajes.')
+# else:
+#     print('Mensajes:')
+#     for message in messages:
+#         # msg = service.users().messages().get(userId='me', id=message['id']).execute()
+#         # print(f"Asunto: {msg['subject']}, De: {msg['from']}")
