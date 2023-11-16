ddfd221362a63cc6b4e379b8f15f9c977d9a434e
jorafael3 <jalvaradoe3@gmail.com>
2023-11-15 23:20:25 -0500

    awd

--- models/dashboardmodel.php
+++ models/dashboardmodel.php
@@ -71,32 +71,34 @@ class dashboardmodel extends Model
         try {
             $inicio_mes = $param["inicio_mes"];
             $fin_mes = $param["fin_mes"];
+            $producto = $param["producto"];
             $query = $this->db->connect_dobra()->prepare("SELECT
             (
-                select  DESCRIPCION  from guias_detalle gd where CODIGO = '10016416'
+                select  DESCRIPCION  from guias_detalle gd where CODIGO = :producto
                 limit 1
             ) as DESCRIPCION,
             (
-                select  UNIDAD  from guias_detalle gd where CODIGO = '10016416'
+                select  UNIDAD  from guias_detalle gd where CODIGO = :producto
                 limit 1
             ) as UNIDAD,
-            (
+            ifnull((
                 select sum(ggdd.CANTIDAD_PARCIAL) + sum(ggdd.CANTIDAD_TOTAL) 
                 from gui_guias_despachadas_dt ggdd
                 left join gui_guias_despachadas_estado ggde 
                 on ggdd.PEDIDO_INTERNO  = ggde.PEDIDO_INTERNO
-                where ggdd.CODIGO = '10016416'
+                where ggdd.CODIGO = :producto
                 and date(ggde.FECHA_CREADO) between 
                 DATE_FORMAT(DATE_ADD(CURDATE(), INTERVAL -1 MONTH), '%Y-%m-01') AND LAST_DAY(DATE_ADD(CURDATE(), INTERVAL -1 MONTH))
-            ) as CANTIDAD_CEMENTO_MES_ANTERIOR,
-            sum(ggdd.CANTIDAD_PARCIAL) + sum(ggdd.CANTIDAD_TOTAL) as CANTIDAD_CEMENTO_MES_ACTUAL
+            ),0) as CANTIDAD_CEMENTO_MES_ANTERIOR,
+            ifnull(sum(ggdd.CANTIDAD_PARCIAL) + sum(ggdd.CANTIDAD_TOTAL),0) as CANTIDAD_CEMENTO_MES_ACTUAL
             from gui_guias_despachadas_dt ggdd 
             left join gui_guias_despachadas_estado ggde 
             on ggdd.PEDIDO_INTERNO  = ggde.PEDIDO_INTERNO
-            where ggdd.CODIGO = '10016416'
+            where ggdd.CODIGO = :producto
             and date(ggde.FECHA_CREADO) between :inicio_mes and :fin_mes ");
             $query->bindParam(":inicio_mes", $inicio_mes, PDO::PARAM_STR);
             $query->bindParam(":fin_mes", $fin_mes, PDO::PARAM_STR);
+            $query->bindParam(":producto", $producto, PDO::PARAM_STR);
             if ($query->execute()) {
                 $result = $query->fetchAll(PDO::FETCH_ASSOC);
                 $mes = $this->Cantidad_Sacos_Mes_Grafico($param);
@@ -116,6 +118,7 @@ class dashboardmodel extends Model
     function Cantidad_Sacos_Mes_Grafico($param)
     {
         try {
+            $producto = $param["producto"];
             $query = $this->db->connect_dobra()->prepare("SELECT
             t1.CODIGO,
             CONCAT(YEAR(t2.FECHA_COMPLETADO), '-', LPAD(MONTH(t2.FECHA_COMPLETADO), 2, '0')) AS AnioMes,
@@ -126,13 +129,13 @@ class dashboardmodel extends Model
                     gui_guias_despachadas_estado t2 ON t1.PEDIDO_INTERNO = t2.PEDIDO_INTERNO
                 WHERE
                     t2.ESTADO_DESPACHO = 0
-                    and t1.CODIGO = '10016416'
+                    and t1.CODIGO = :producto
                 GROUP BY
             t1.CODIGO,
             YEAR(t2.FECHA_COMPLETADO),
             MONTH(t2.FECHA_COMPLETADO);
            ");
-            // $query->bindParam(":pedido", $PEDIDO, PDO::PARAM_STR);
+            $query->bindParam(":producto", $producto, PDO::PARAM_STR);
             if ($query->execute()) {
                 $result = $query->fetchAll(PDO::FETCH_ASSOC);
                 return $result;
@@ -152,6 +155,8 @@ class dashboardmodel extends Model
         try {
             $inicio_mes = $param["inicio_mes"];
             $fin_mes = $param["fin_mes"];
+            $producto = $param["producto"];
+
             $query = $this->db->connect_dobra()->prepare("SELECT 
             uc.usuario_id,
             uu.Nombre,
@@ -160,7 +165,7 @@ class dashboardmodel extends Model
             from gui_guias_despachadas_dt ggdd 
             left join gui_guias_despachadas_estado ggde 
             on ggde.PEDIDO_INTERNO = ggdd.PEDIDO_INTERNO 
-            where ggde.CREADO_POR = uu.Usuario_ID  and ggdd.CODIGO ='10016416'
+            where ggde.CREADO_POR = uu.Usuario_ID  and ggdd.CODIGO =:producto
             and date(ggde.FECHA_CREADO) between :inicio_mes and :fin_mes 
             ),0)as CANT_CEMENTO
             from us_choferes uc 
@@ -170,9 +175,10 @@ class dashboardmodel extends Model
            ");
             $query->bindParam(":inicio_mes", $inicio_mes, PDO::PARAM_STR);
             $query->bindParam(":fin_mes", $fin_mes, PDO::PARAM_STR);
+            $query->bindParam(":producto", $producto, PDO::PARAM_STR);
             if ($query->execute()) {
                 $result = $query->fetchAll(PDO::FETCH_ASSOC);
-                $mes = $this->Chofer_Mas_Despachos_Grafico($result[0]["usuario_id"]);
+                $mes = $this->Chofer_Mas_Despachos_Grafico($result[0]["usuario_id"],$param);
                 return array("DATOS" => $result, "GRAFICO" =>  $mes);
             } else {
                 $err = $query->errorInfo();
@@ -185,9 +191,10 @@ class dashboardmodel extends Model
         }
     }
 
-    function Chofer_Mas_Despachos_Grafico($param)
+    function Chofer_Mas_Despachos_Grafico($usuario,$param)
     {
         try {
+            $producto = $param["producto"];
             $query = $this->db->connect_dobra()->prepare("SELECT
             t1.CODIGO,
             t2.CREADO_POR,
@@ -196,11 +203,13 @@ class dashboardmodel extends Model
             from gui_guias_despachadas_dt t1 
             join gui_guias_despachadas_estado t2 
             on t1.PEDIDO_INTERNO = t2.PEDIDO_INTERNO
-            where t1.CODIGO = '10016416'
+            where t1.CODIGO = :producto
             and t2.CREADO_POR = :usuario
             group by MES
            ");
-            $query->bindParam(":usuario", $param, PDO::PARAM_STR);
+            $query->bindParam(":usuario", $usuario, PDO::PARAM_STR);
+            $query->bindParam(":producto", $producto, PDO::PARAM_STR);
+
             if ($query->execute()) {
                 $result = $query->fetchAll(PDO::FETCH_ASSOC);
                 return $result;
@@ -220,6 +229,8 @@ class dashboardmodel extends Model
         try {
             $inicio_mes = $param["inicio_mes"];
             $fin_mes = $param["fin_mes"];
+            $producto = $param["producto"];
+
             $query = $this->db->connect_dobra()->prepare("SELECT 
             date(ggde.FECHA_COMPLETADO) as FECHA,
             ggdd.CODIGO,
@@ -229,11 +240,13 @@ class dashboardmodel extends Model
             left join gui_guias_despachadas_dt ggdd 
             on ggde.PEDIDO_INTERNO = ggdd.PEDIDO_INTERNO
             where date(ggde.FECHA_COMPLETADO) between :inicio_mes and :fin_mes
-            and CODIGO = '10016416'
+            and CODIGO = :producto
             group by FECHA 
            ");
             $query->bindParam(":inicio_mes", $inicio_mes, PDO::PARAM_STR);
             $query->bindParam(":fin_mes", $fin_mes, PDO::PARAM_STR);
+            $query->bindParam(":producto", $producto, PDO::PARAM_STR);
+
             if ($query->execute()) {
                 $result = $query->fetchAll(PDO::FETCH_ASSOC);
                 return array("DATOS" => $result);
@@ -253,6 +266,8 @@ class dashboardmodel extends Model
         try {
             $inicio_mes = $param["inicio_mes_a"];
             $fin_mes = $param["fin_mes_a"];
+            $producto = $param["producto"];
+
             $query = $this->db->connect_dobra()->prepare("SELECT 
             date(ggde.FECHA_COMPLETADO) as FECHA_ANT,
             ggdd.CODIGO,
@@ -262,11 +277,13 @@ class dashboardmodel extends Model
             left join gui_guias_despachadas_dt ggdd 
             on ggde.PEDIDO_INTERNO = ggdd.PEDIDO_INTERNO
             where date(ggde.FECHA_COMPLETADO) between :inicio_mes and :fin_mes
-            and CODIGO = '10016416'
+            and CODIGO = :producto
             group by FECHA_ANT 
            ");
             $query->bindParam(":inicio_mes", $inicio_mes, PDO::PARAM_STR);
             $query->bindParam(":fin_mes", $fin_mes, PDO::PARAM_STR);
+            $query->bindParam(":producto", $producto, PDO::PARAM_STR);
+
             if ($query->execute()) {
                 $result = $query->fetchAll(PDO::FETCH_ASSOC);
                 return array("DATOS" => $result);
@@ -286,16 +303,18 @@ class dashboardmodel extends Model
         try {
             // $inicio_mes = $param["inicio_mes"];
             // $fin_mes = $param["fin_mes"];
+            $producto = $param["producto"];
             $query = $this->db->connect_dobra()->prepare("SELECT
-            gd.CODIGO,
-            concat(year(STR_TO_DATE(g.FECHA_DE_EMISION , '%d.%m.%Y')),'-',lpad(month(STR_TO_DATE(g.FECHA_DE_EMISION , '%d.%m.%Y')),2,'0')) as FECHA,
-            sum(gd.POR_DESPACHAR) AS cantidad
-            from guias_detalle gd 
-            join guias g 
-            on gd.PEDIDO_INTERNO = g.PEDIDO_INTERNO
-            where gd.CODIGO = '10016416'
+            t1.CODIGO,
+            concat(year(t2.FECHA_CREADO),'-',lpad(month(t2.FECHA_CREADO),2,'0')) as FECHA,
+            sum(t1.CANTIDAD_PARCIAL) + sum(t1.CANTIDAD_TOTAL) as cantidad
+            from gui_guias_despachadas_dt t1 
+            join gui_guias_despachadas_estado t2 
+            on t1.PEDIDO_INTERNO = t2.PEDIDO_INTERNO
+            where t1.CODIGO = :producto
             group by FECHA
            ");
+            $query->bindParam(":producto", $producto, PDO::PARAM_STR);
             // $query->bindParam(":inicio_mes", $inicio_mes, PDO::PARAM_STR);
             // $query->bindParam(":fin_mes", $fin_mes, PDO::PARAM_STR);
             if ($query->execute()) {
@@ -317,6 +336,7 @@ class dashboardmodel extends Model
         try {
             // $inicio_mes = $param["inicio_mes"];
             // $fin_mes = $param["fin_mes"];
+            $producto = $param["producto"];
             $query = $this->db->connect_dobra()->prepare("SELECT
             gd.CODIGO,
             gd.PEDIDO_INTERNO,
@@ -325,9 +345,11 @@ class dashboardmodel extends Model
             from guias_detalle gd 
             join guias g 
             on gd.PEDIDO_INTERNO = g.PEDIDO_INTERNO
-            where gd.CODIGO = '10016416'
+            where gd.CODIGO = :producto
             group by FECHA
            ");
+            $query->bindParam(":producto", $producto, PDO::PARAM_STR);
+
             // $query->bindParam(":inicio_mes", $inicio_mes, PDO::PARAM_STR);
             // $query->bindParam(":fin_mes", $fin_mes, PDO::PARAM_STR);
             if ($query->execute()) {
